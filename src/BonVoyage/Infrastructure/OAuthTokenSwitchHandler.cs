dbf8bfb84a8bf4021042cd09a4d1c06bdfc29906
Tugberk Ugurlu <tugberk@outlook.com>
2017-05-23 22:33:15 +0100

    do that only when we have the value

--- src/BonVoyage/Infrastructure/OAuthTokenSwitchHandler.cs
+++ src/BonVoyage/Infrastructure/OAuthTokenSwitchHandler.cs
@@ -11,14 +11,17 @@ namespace BonVoyage.Infrastructure
 
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var accessToken = request.Headers.Authorization.Parameter;
-            var queryStringValue = $"{OAuthQueryStringParameter}={accessToken}";
+            if (request.Headers.Authorization != null)
+            {
+                var accessToken = request.Headers.Authorization.Parameter;
+                var queryStringValue = $"{OAuthQueryStringParameter}={accessToken}";
 
-            request.RequestUri = string.IsNullOrWhiteSpace(request.RequestUri.Query)
-                ? new Uri(string.Concat(request.RequestUri.ToString(), "?", queryStringValue))
-                : new Uri(string.Concat(request.RequestUri.ToString(), "&", queryStringValue));
+                request.RequestUri = string.IsNullOrWhiteSpace(request.RequestUri.Query)
+                    ? new Uri(string.Concat(request.RequestUri.ToString(), "?", queryStringValue))
+                    : new Uri(string.Concat(request.RequestUri.ToString(), "&", queryStringValue));
 
-            request.Headers.Authorization = null;
+                request.Headers.Authorization = null;
+            }
 
             return base.SendAsync(request, cancellationToken);
         }
