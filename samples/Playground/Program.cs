83360b71beb9ff7ad206c70cbce2e95738ed1f37
Tugberk Ugurlu <tugberk@outlook.com>
2017-05-24 00:18:51 +0100

    added the playground sample

---
+++ samples/Playground/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BonVoyage;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ClientId:");
+            var clientId = Console.ReadLine();
+
+            Console.WriteLine("ClientSecret:");
+            var clientSecret = Console.ReadLine();
+
+            using (var bonVoyageContext = new BonVoyageContext())
+            using (var foursquareContext = bonVoyageContext.CreateUserlessFoursquareContext(new UserlessAccessSettings(clientId, clientSecret)))
+            {
+                var categories = foursquareContext.Categories.Get().Result;
+
+                foreach (var venueCategory in categories)
+                {
+                    Console.WriteLine("{0}: {1}", venueCategory.Id, venueCategory.Name);
+                }
+            }
+        }
+    }
+}
