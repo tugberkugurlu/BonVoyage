fc98cd8811b6845843679f59169475cae5dec16d
Tugberk Ugurlu <tugberk@outlook.com>
2017-05-23 21:25:10 +0100

    added test project

---
+++ tests/BonVoyage.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace BonVoyage.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
