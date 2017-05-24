ce5fa90ecfbb8db7abdecf5e8f84d641173dbadd
Tugberk Ugurlu <tugberk@outlook.com>
2017-05-24 21:34:25 +0100

    added nuget push script
    which will not work as we don't have the API key

---
+++ scripts/push-to-nuget.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+for i in "$@"
+do
+case $i in
+    -k=*|--apikey=*)
+    APIKEY="${i#*=}"
+    shift # past argument=value
+    ;;
+    -p=*|--packagePath=*)
+    PACKAGEPATH="${i#*=}"
+    shift # past argument=value
+    ;;
+    -v=*|--version=*)
+    VERSION="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+done
+
+if [ -z ${APIKEY+x} ]
+then
+    echo "Error: ApiKey needs to be specified through --apikey|-k parameter"
+    exit 1
+fi
+
+if [ -z ${PACKAGEPATH+x} ]
+then
+    echo "Error: PackagePath needs to be specified through --packagePath|-p parameter"
+    exit 1
+fi
+
+if [ -z ${VERSION+x} ]
+then
+    echo "Error: Version needs to be specified through --version|-v parameter"
+    exit 1
+fi
+
+echo "pushing $packagePath to NuGet.org..."
+dotnet nuget push $PACKAGEPATH --api-key $APIKEY --force-english-output || exit 1
