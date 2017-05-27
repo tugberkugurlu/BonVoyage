0087a4f6ba8713f87026c37e651bae6e3073e8e3
Tugberk Ugurlu <tugberk@outlook.com>
2017-05-27 12:37:17 +0100

    GetPhotos extension on CompactVenue

---
+++ src/BonVoyage/CompactVenueExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BonVoyage.Clients;
+using BonVoyage.Models;
+
+namespace BonVoyage
+{
+    public static class CompactVenueExtensions
+    {
+        /// <summary>
+        /// Returns first 50 photos for a venue.
+        /// </summary>
+        /// <param name="venue">The venue you want photos for.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static Task<IReadOnlyCollection<VenuePhoto>> GetPhotos(this CompactVenue venue)
+        {
+            if (venue == null) throw new ArgumentNullException(nameof(venue));
+            
+            var client = new PhotosClient(venue.HttpClient);
+            return client.GetVenuePhotos(venue.Id, 50, 0);
+        }
+    }
+}
